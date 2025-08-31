53fb245ccaae02206632969c2b54315399c93902
sowmiya6498 <sowmiyasaravanan6498@gmail.com>
2025-08-31 17:27:01 +0530

    Update server.js

--- microservice/server.js
+++ microservice/server.js
@@ -1,1 +1,11 @@
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from microservice-blue!");
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
